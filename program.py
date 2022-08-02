4b639b6c5adb253bc28d6debc1f937b7cf65da0d
Якупов Альберт <folietoy@yandex.com>
2022-08-03 03:53:55 +0500

    test 4

--- program.py
+++ program.py
@@ -1,3 +1,7 @@
-print('proverka 1')
-print('proverka 2')
-print('proverka 3')
+def get_mean(values):
+    result = (sum(values) / len(values))
+    return f'{result:.2f}'
+# Список значений для теста
+num_lst = [3, 6, 5, 7, 9, 1]
+# Тут вызов функции.
+print(get_mean(num_lst))  # Напечатайте результат вызова функции.
